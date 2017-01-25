f635e60d6bc46f6f48d5bd0a6fddc84d0b83aa76
sleepwalky <we.alexfriend@gmail.com>
2017-01-26 01:04:41 +0300

    extend routes

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -17,8 +17,66 @@ const routes = function (Todo) {
                     res.json(todos);
                 }
             })
+        });
+
+    todoRouter.use('/:todoId', function (req, res, next) {
+        Todo.findById(req.params.todoId, function (err, todo) {
+            if (err)
+                res.status(500).send(err);
+            else if (todo) {
+                req.todo = todo;
+                next();
+            } else {
+                res.status(404).send('no todo found');
+            }
+        });
+    });
+    todoRouter.route('/:todoId')
+        .get(function (req, res) {
+            res.json(req.todo);
         })
-        return todoRouter;
+        .put(function (req, res) {
+            req.todo.name = req.body.name;
+            req.todo.descr = req.body.descr;
+            req.todo.hours = req.body.hours;
+            req.todo.type = req.body.type;
+            req.todo.save(function (err) {
+                if (err)
+                    res.status(500).send(err);
+                else {
+                    res.json(req.todo);
+                }
+            });
+        })
+        .patch(function (req, res) {
+            if (req.body._id) {
+                delete req.body._id;
+            }
+                
+
+            for (let p in req.body) {
+                req.todo[p] = req.body[p];
+            }
+
+            req.todo.save(function (err) {
+                if (err)
+                    res.status(500).send(err);
+                else {
+                    res.json(req.todo);
+                }
+            });
+        })
+        .delete(function (req, res) {
+            req.todo.remove(function (err) {
+                if (err)
+                    res.status(500).send(err);
+                else {
+                    res.status(204).send('Removed');
+                }
+            });
+        });
+
+    return todoRouter;
 }
 
 module.exports = routes;
